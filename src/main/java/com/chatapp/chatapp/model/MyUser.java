a8fa5bc6f56c321110af3833ca263e87081cb582
Mahady Hasan Fahim <fmahady01@gmail.com>
2024-07-09 10:58:21 +0600

    Update the user details

--- src/main/java/com/chatapp/chatapp/model/MyUser.java
+++ src/main/java/com/chatapp/chatapp/model/MyUser.java
@@ -5,50 +5,54 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+
 @Entity
+
 public class MyUser {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
     private String username;
     private String password;
-    private String role; //Eg: ADMIN,USER
+    private String role; 
 
+    
     @Override
     public String toString() {
-        return "MyUser [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
+        return "MyUser [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + ", role="
+                + role + "]";
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
-
     public String getRole() {
         return role;
     }
-
     public void setRole(String role) {
         this.role = role;
     }
+
 }
